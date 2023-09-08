52dd781af95b5ab03f72b1bfd9f7faeaee4b48d2
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-09-08 19:03:29 +0200

    Aggiungere i file di progetto.

---
+++ Prodotto.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace csharp_oop_shop
+{
+
+    internal class Prodotto
+    {
+     
+        private int code;
+        private string name;
+        private string description;
+        private float price;
+        private int vat = 22;
+
+
+
+
+        //GETTER
+
+        public int GetCode()
+        {
+            return this.code;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetDescription()
+        {
+            return this.description;
+        }
+
+        public float GetPrice()
+        {
+            return this.price;
+        }
+
+        public int GetVat()
+        {
+            return this.vat;
+        }
+
+
+
+
+        //SETTER
+
+        public string SetName(string value)
+        {
+            return value;
+        }
+
+        public string SetDescription(string value)
+        {
+            return value;
+        }
+
+        public float SetPrice(float value)
+        {
+            return value;
+        }
+
+        public int SetVat(int value)
+        {
+            return value;
+        }
+
+
+
+
+
+        //METODI
+
+        public float basePrice()
+        {
+            return price;
+        }
+
+        public float vatPrice() 
+        {
+            float vatPrice = (price * vat / 100) + price;
+            return vatPrice;
+        }
+
+        public string extendedName()
+        {
+            string extendedName = code + name;
+            return extendedName;
+        }
+
+        // Metodi Bonus
+
+        public string codePadLeft()
+        {
+            string newCode = code.ToString();
+            newCode = code.ToString().PadLeft(8, '0');
+            return newCode;
+        }
+
+
+
+
+        //COSTRUTTORI
+
+        public Prodotto(string name, string description, float price)
+        {
+            Random rnd = new Random();
+            this.code = rnd.Next(0, 999999);
+            this.name = name;
+            this.description = description;
+            this.price = price;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharp_oop_shop;
+
+
+Prodotto newProduct = new Prodotto("scarpe", "Scarpa donna con intersuola abzorb firmata New balance", 50);
+Console.WriteLine($"Nome prodotto: {newProduct.GetName()}");
+Console.WriteLine($"Descrizione prodotto: {newProduct.GetDescription()}");
+Console.WriteLine($"Prezzo prodotto: {newProduct.GetPrice()} euro");
+Console.WriteLine($"Prezzo prodotto: {newProduct.basePrice()} euro");
+Console.WriteLine($"Prezzo con iva: {newProduct.vatPrice()} euro");
+Console.WriteLine($"Nome per esteso: {newProduct.extendedName()}");
+
+Console.WriteLine("-------------------------");
+
+Prodotto newProduct1 = new Prodotto("Pullover", "Pullover con collo rotondo e maniche lunghe. Orlo con spacchi laterali. Finiture a coste.", 15.95f);
+Console.WriteLine($"Nome prodotto: {newProduct1.GetName()}");
+Console.WriteLine($"Descrizione prodotto: {newProduct1.GetDescription()}");
+Console.WriteLine($"Prezzo prodotto: {newProduct1.GetPrice()} euro");
+Console.WriteLine($"Prezzo prodotto: {newProduct1.basePrice()} euro");
+Console.WriteLine($"Prezzo al netto di iva: {newProduct1.vatPrice()} euro");
+Console.WriteLine($"Nome per esteso: {newProduct1.extendedName()}");
+Console.WriteLine($"codice pad left: {newProduct1.codePadLeft()}");
